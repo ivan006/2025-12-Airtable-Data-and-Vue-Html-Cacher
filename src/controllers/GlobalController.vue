11fdd43982cbbf978bf8d10691c39c1da84871ae
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-12-15 13:17:21 +0200

    1

--- src/controllers/GlobalController.vue
+++ src/controllers/GlobalController.vue
@@ -1,205 +1,37 @@
 <template>
-  <div
-  >
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Cache Manager
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
 
-    <!-- TOP CONTACT BAR -->
-    <!-- 🔥 TOP CONTACT + SOCIAL BAR -->
-    <!-- 🔥 TOP CONTACT + SOCIAL BAR (taller version) -->
-    <div
-      class="bg-white"
-      style="
-        border-bottom: 1px solid rgba(0,0,0,0.08);
-        padding: 10px 0;       /* taller */
-      "
-    >
-      <div class="container-md">
-        <div class="row justify-between items-center">
+    <q-drawer show-if-above bordered>
+      <q-list padding>
 
-          <!-- LEFT: PHONE -->
-          <div class="row items-center q-gutter-sm">
+        <q-item clickable to="/data-cache-info">
+          <q-item-section>Data Cache (How It Works)</q-item-section>
+        </q-item>
 
-            <q-icon
-              name="mdi-phone-outline"
-              size="20px"
-              style="opacity: 0.85;"
-            />
+        <q-item clickable to="/data-cache-binder">
+          <q-item-section>Data Cache (Page Binder)</q-item-section>
+        </q-item>
 
-            <span
-              class="text-body1"
-              style="font-weight: 500; letter-spacing: 0.3px;"
-            >
-              {{ this.item.fields?.['Phone Number'] }}
-            </span>
+        <q-item clickable to="/html-cache-pages">
+          <q-item-section>HTML Cache (Page List)</q-item-section>
+        </q-item>
 
-          </div>
-
-          <!-- RIGHT: SOCIAL -->
-          <div class="row items-center q-gutter-lg">
-
-            <a :href="this.item.fields?.['Facebook URL']" target="_blank" style="color: #000;">
-              <q-icon name="mdi-facebook" size="20px" style="opacity: 0.85;" />
-            </a>
-
-            <a :href="this.item.fields?.['Instagram URL']" target="_blank" style="color: #000;">
-              <q-icon name="mdi-instagram" size="20px" style="opacity: 0.85;" />
-            </a>
-
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-
-
-
-    <q-toolbar class="q-py-md q-px-none bg-whit text-" style="margin-bottom: -0px; background-color: rgba(255,255,255,1.0); border-bottom: 1px solid rgba(0, 0, 0, 0.12);">
-
-      <div class="container-md  ">
-        <div v-show="loading || childLoading">
-          <div class="text-center q-pa-xl">Loading...</div>
-        </div>
-        <div v-show="!(loading || childLoading)">
-          <div class="row justify-between items-center  q-py-mdx ">
-
-            <div
-              class="col-12x col-md-auto text-center"
-            >
-
-              <!--<q-btn flat round dense icon="menu" class="q-mr-sm" />-->
-              <!--<q-avatar large>-->
-              <!--  <img :src="this.item.fields?.['Logo Image']?.[0]?.url">-->
-              <!--  &lt;!&ndash;<q-icon name="school" size="lg" style="opacity: 50%" />&ndash;&gt;-->
-              <!--</q-avatar>-->
-
-
-
-              <q-item
-                to="/"
-                class="q-pa-none"
-              >
-
-                <q-item-section>
-
-                  <div>
-                    <img
-                      :src="this.item.fields?.['Logo Image'][0].thumbnails.large.url ? `https://capetownlists.co.za/?url=${encodeURIComponent(this.item.fields?.['Logo Image'][0].thumbnails.large.url)}` : ''"
-                      style="max-height: 100px;"
-                    >
-                  </div>
-                </q-item-section>
-              </q-item>
-              <!--<q-toolbar-title>-->
-              <!--  &lt;!&ndash;{{ siteTitle }}&ndash;&gt;-->
-              <!--</q-toolbar-title>-->
-            </div>
-            <div
-              class="col-12x col-md-auto q-my-lgx text-center"
-            >
-              <!-- <MegaMenu /> -->
-
-              <!--<q-btn flat round dense icon="whatshot" />-->
-              <MenuItems @loaded="childLoading=false" />
-
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-    </q-toolbar>
-
-
-
-    <!--<img v-if="item.fields" :src="`https://capetownlists.co.za/?url=${encodeURIComponent(item.fields['Site Background Image']?.[0]?.url)}`" alt="">-->
-    <!--<img v-if="item.fields" :src="`${item.fields['Site Background Image']?.[0]?.url}`" alt="">-->
-
-
+      </q-list>
+    </q-drawer>
 
     <q-page-container>
-      <!--<pre>{{item}}</pre>-->
-      <div class=" " style="min-height: 60vh;">
-        <!--<BreadcrumbsComp />-->
-        <router-view />
-      </div>
+      <router-view />
     </q-page-container>
-    <!--<div class="q-pb-md">-->
-    <!--</div>-->
-    <!--<div class="q-px-md">-->
-    <!--</div>-->
-    
-    <DataCacheManager />
-    <FooterController />
-  </div>
-
+  </q-layout>
 </template>
 
-<script>
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-// import BreadcrumbsComp from 'src/controllers/BreadcrumbsComp.vue';
-import VueCookies from 'vue-cookies';
-import MenuItems from 'src/controllers/MenuItems.vue';
-import Site_Menu_Items from "src/models/orm-api/Site_Menu_Items";
-import Site from "src/models/orm-api/Site";
-import FooterController from "src/controllers/FooterController.vue";
-import DataCacheManager from 'src/controllers/DataCacheManager.vue'
-import MegaMenu from 'src/controllers/MegaMenu.vue'
-
-
-export default {
-  name: 'GlobalController',
-  components: {
-    FooterController,
-    // BreadcrumbsComp,
-    MenuItems,
-    DataCacheManager,
-    // MegaMenu,
-  },
-
-  data(){
-    return {
-      siteTitle: 'Lorum Ipsum',
-      loading: true,
-      childLoading: false,
-      item: {},
-    }
-  },
-  computed: {
-
-    id() {
-      // return this.$route.params.rId
-      return 'reci1Y5KdKFBkz3T1'
-    },
-    superTableModel() {
-      return Site
-    },
-    bgUrl() {
-      const result = this.item.fields?.['Site Background Image']?.[0]?.url
-      return result
-    },
-  },
-  methods: {
-    fetchData() {
-      this.loading = true
-      this.superTableModel
-        .FetchById(
-          this.id,
-          // this.relationships,
-          [],
-          { flags: {}, moreHeaders: {}, rels: [] }
-        )
-        .then((response) => {
-          this.item = response.response.data
-          this.loading = false
-        })
-        .catch(() => {
-          this.loading = false
-        });
-    },
-  },
-  mounted(){
-    this.fetchData();
-  }
-};
+<script setup>
+// no logic needed yet
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,122 +1,31 @@
 const routes = [
   {
     path: '/',
-    component: () => import('src/controllers/EmptyLayout.vue'),
+    component: () => import('src/controllers/GlobalController.vue'),
     meta: { requiresAuth: false },
     children: [
       {
-        path: '/sitemap-artworks',
-        name: '/sitemap-artworks',
-        component: () => import('src/controllers/SitemapArtworks.vue'),
-        meta: {
-          breadcrumbName: 'Home',
-          breadcrumbParentName: '',
-          requiresAuth: false,
-        },
+        path: '',
+        redirect: '/data-cache-info'
       },
       {
-        path: '/',
-        component: () => import('src/controllers/GlobalController.vue'),
-        // redirect: to => { return '/home' },
-        children: [
-          {
-            path: '/',
-            name: '/',
-            component: () => import('src/controllers/HomeController.vue'),
-            meta: {
-              breadcrumbName: 'Home',
-              breadcrumbParentName: '',
-              requiresAuth: false,
-            },
-          },
-          {
-            path: '/home-old',
-            name: '/home-old',
-            component: () => import('src/controllers/HomeOldComp.vue'),
-            meta: {
-              breadcrumbName: 'Home',
-              breadcrumbParentName: '',
-              requiresAuth: false,
-            },
-          },
-          {
-            path: '/home-old-2',
-            name: '/home-old-2',
-            component: () => import('src/controllers/HomeOld2Comp.vue'),
-            meta: {
-              breadcrumbName: 'Home',
-              breadcrumbParentName: '',
-              requiresAuth: false,
-            },
-          },
-          {
-            path: '/artwork-old',
-            name: '/artwork-old',
-            component: () => import('src/controllers/Secondary_Page_Controller.vue'),
-            meta: {
-              breadcrumbName: 'Gas Prices',
-              breadcrumbParentName: '',
-              requiresAuth: false,
-            },
-          },
-          {
-            path: '/all-artworks/:medium/:priceRange',
-            name: '/all-artworks/:medium/:priceRange',
-            component: () => import('src/controllers/ArtworksPageComp.vue'),
-            meta: {
-              breadcrumbName: 'Artworks',
-              breadcrumbParentName: '',
-              requiresAuth: false,
-            },
-          },
-          // {
-          //   path: '/artworks',
-          //   name: '/artworks',
-          //   component: () => import('src/controllers/ArtworksPageComp.vue'),
-          //   meta: {
-          //     breadcrumbName: 'Artworks',
-          //     breadcrumbParentName: '',
-          //     requiresAuth: false,
-          //   },
-          // },
-          {
-            path: '/artworks/:rId/:rName',
-            name: '/artworks/:rId/:rName',
-            // path: '/home/:rId',
-            // name: '/home/:rId',
-            component: () => import('src/controllers/ArtworkComp.vue'),
-            meta: {
-              breadcrumbName: ':rId',
-              breadcrumbParentName: '/artworks',
-              requiresAuth: false,
-            },
-          },
-          {
-            // path: '/all-artists/:artistType/:artistLevel',
-            path: '/all-artists/:artistType',
-            name: '/all-artists/:artistType',
-            component: () => import('src/controllers/ArtistsPageComp.vue'),
-            meta: {
-              breadcrumbName: '',
-              breadcrumbParentName: '',
-              requiresAuth: false,
-            },
-          },
-          {
-            path: '/artists/:rId/:rName',
-            name: '/artists/:rId/:rName',
-            component: () => import('src/controllers/ArtistComp.vue'),
-            meta: {
-              breadcrumbName: ':rId',
-              breadcrumbParentName: '/artists',
-              requiresAuth: false,
-            },
-          },
-        ],
+        path: 'data-cache-info',
+        component: () => import('src/controllers/DataCacheInfo.vue'),
+        meta: { requiresAuth: false }
+      },
+      {
+        path: 'data-cache-binder',
+        component: () => import('src/controllers/DataCacheBinder.vue'),
+        meta: { requiresAuth: false }
+      },
+      {
+        path: 'html-cache-pages',
+        component: () => import('src/controllers/HtmlCachePages.vue'),
         meta: { requiresAuth: false }
       }
     ]
   },
+
   {
     path: '/:catchAll(.*)*',
     component: () => import('src/controllers/ErrorNotFound.vue'),
